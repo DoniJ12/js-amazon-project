2f584f0b9474d183c4abe37c78d6c5725018ec37
Doni J <donijosy67@gmail.com>
2024-08-29 11:30:12 +0300

    using function for add to cart and import using module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,5 @@
-import {cart} from '../data/cart.js';
-import { products } from '../data/products.js';
+import { cart, addToCart } from "../data/cart.js";
+import { products } from "../data/products.js";
 let productsHtml = "";
 products.forEach((product) => {
   productsHtml += `
@@ -55,29 +55,20 @@ products.forEach((product) => {
         </div>
         `;
 });
+
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  });
+}
 document.querySelector(".products-grid").innerHTML = productsHtml;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
-
-    let matchingItem;
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    addToCart(productId);
+    updateCartQuantity();
   });
 });
