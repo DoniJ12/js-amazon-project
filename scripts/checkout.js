aaa05ada7a1a451b99e487648d5b7756e4013558
Doni J <donijosy67@gmail.com>
2024-08-30 13:39:56 +0300

    functional delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -101,6 +101,5 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
   });
 });
