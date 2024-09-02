d86c329b85d90ec9f9db386a10805e32cdbba71d
Doni J <donijosy67@gmail.com>
2024-09-02 10:14:35 +0300

    Update delivery option (interactive)

--- data/cart.js
+++ data/cart.js
@@ -5,12 +5,12 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
-      deliveryOptionId: '1',
+      deliveryOptionId: "1",
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
-      deliveryOptionId: '2',
+      deliveryOptionId: "2",
     },
   ];
 }
@@ -32,7 +32,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
-      deliveryOptionId: '1',
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
@@ -46,5 +46,16 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
+}
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
   saveToStorage();
 }
